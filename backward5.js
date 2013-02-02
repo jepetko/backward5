02b5fc5e1ca056cd01e075e301c19ff262b9611d
katarina <golbang.k@gmail.com>
2013-02-02 22:04:57 +0100

    do it jquery way

--- backward5.js
+++ backward5.js
@@ -1,67 +1,66 @@
-var backward5 = function() {
-    return new (function workarounds() {
-        function $id(a) {
-            return "#"+a;
-        };
+(function($) {
+    $.fn.backward5 = function() {
         //provide alternative support for following input types:
         //range, date, color, email, month, number, search, tel, time, url, week, datetime, datetime-local
-        this.range = function() {
-            var _id = "div_" + this.id;
-            $( $id(this.id) ).replaceWith('<div id="' + _id + '"/>');
-            $( $id(_id) ).slider( {max:parseInt(this.max), min:parseInt(this.min)} );
+        var workarounds = {
+            range : function() {
+                var _id = "div_" + this.id;
+                $( this ).replaceWith('<div id="' + _id + '"/>');
+                $( this ).slider( {max:parseInt(this.max), min:parseInt(this.min)} );
+            },
+            date : function() {
+                $( this ).datepicker();
+            },
+            color : function() {
+                $( this ).colorPicker();
+            },
+            email : function() {
+                //support this
+            },
+            month : function() {
+                //support this
+            },
+            number : function() {
+                $( this ).spinner();
+            },
+            search : function() {
+                //support this
+            },
+            tel : function() {
+                //support this
+            },
+            time : function() {
+                //support this
+            },
+            url : function() {
+                //support this
+            },
+            week : function() {
+                //support this
+            },
+            datetime : function() {
+                //support this
+            }
         };
-        this.date = function() {
-            $( $id(this.id) ).datepicker();
-        };
-        this.color = function() {
-            $( $id(this.id)).colorPicker();
-        };
-        this.email = function() {
-            //support this
-        };
-        this.month = function() {
-            //support this
-        };
-        this.number = function() {
-            $( $id(this.id) ).spinner();
-        };
-        this.search = function() {
-            //support this
-        };
-        this.tel = function() {
-            //support this
-        };
-        this.time = function() {
-            //support this
-        };
-        this.url = function() {
-            //support this
-        };
-        this.week = function() {
-            //support this
-        };
-        this.datetime = function() {
-            //support this
-        };
-        var inputs = arguments[0] || [];
-        var debug = arguments[1] || false;
+
+        var debug = arguments[0] || false;
         var i=0;
-        while( i<inputs.length ) {
-            var input = inputs[i];
-            if( input.type == "text") {
+        while( i<this.length ) {
+            var input = this[i];
+            if( input.type && input.type == "text") {
                 var attr = input.getAttribute("data-mytype");
-                if( attr && attr != "text" && this[attr]) {
+                if( attr && attr != "text" && workarounds[attr]) {
                     if( debug ) {
-                        var p = $( $id(input.id) ).parent();
+                        var p = $( input ).parent();
                         if(p) {
                             p.css("background-color","#90ee90");
                             p.css("border","1px solid #00ff00");
                         }
                     }
-                    this[attr].apply(input,[]);
+                    workarounds[attr].apply(input,[]);
                 }
             }
             i++;
-        };
-    })($("input"),true);
-};
+        }
+    };
+})(jQuery);
