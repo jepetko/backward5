d26c3f2cbcea09d8db84115e085c5ef1abf3ce2a
katarina <golbang.k@gmail.com>
2013-02-03 00:25:04 +0100

    code refactoring

--- backward5.js
+++ backward5.js
@@ -2,54 +2,59 @@
     $.fn.backward5 = function() {
         //provide alternative rendering support for following input types:
         //range, date, color, number
-        var renderStrategies = {
-            range : function() {
-                var _id = "div_" + this.id;
-                $( this ).replaceWith('<div id="' + _id + '/>')
-                return $( "#" + _id ).slider( {max:parseInt(this.max), min:parseInt(this.min)} );
-            },
-            date : function() {
-                return $( this ).datepicker();
-            },
-            color : function() {
-                var el = $( this );
-                if( typeof el.colorPicker == 'function') {
-                    return el.colorPicker();
+        var cfg = {
+            renderStrategies:{
+                range:function () {
+                    var _id = "div_" + this.id;
+                    $(this).replaceWith('<div id="' + _id + '"/>');
+                    return $("#" + _id).slider({max:parseInt(this.max), min:parseInt(this.min)});
+                },
+                date:function () {
+                    return $(this).datepicker();
+                },
+                color:function () {
+                    var el = $(this);
+                    if (typeof el.colorPicker == 'function') {
+                        return el.colorPicker();
+                    }
+                    return null;
+                },
+                number:function () {
+                    return $(this).spinner();
                 }
-                return null;
             },
-            number : function() {
-                return $( this ).spinner();
-            }
+            //some debug styles to give the user feedback
+            debug:arguments[0] || false,
+            debugStyles:[
+                { 'background-color':'#90ee90', 'border':'1px solid #00ff00' },
+                { 'background-color':'#ffffe0', 'border':'1px solid #ffff00' }
+            ]
         };
-        //provide alternative validating support for following input types:
-        //range, date, color, email, month, number, search, tel, time, url, week, datetime, datetime-local
 
-        //do the work:
-        var debug = arguments[0] || false;
-        var debugStyles = [ { 'background-color' : '#90ee90', 'border' : '1px solid #00ff00' },
-                            { 'background-color' : '#ffffe0', 'border' : '1px solid #ffff00' } ];
-        var i=0;
-        while( i<this.length ) {
-            var input = this[i];
-            if( input.type && input.type == "text") {
-                var attr = input.getAttribute("data-bw-type");
-                if( attr && attr != "text" && renderStrategies[attr]) {
-                    var ret = renderStrategies[attr].apply(input,[]);
-                    if( debug ) {
-                        var p = $(input).parent();
-                        var s = debugStyles[1];
-                        if( ret ) {
-                            p = ret.parent();
-                            s = debugStyles[0];
-                        }
-                        for( var styleProp in s ) {
-                            p.css(styleProp, s[styleProp]);
+        this.each( function(cfg) {
+            return function() {
+                if( this.nodeName.toUpperCase() != "INPUT" ) return;
+                if( this.type && this.type == "text") {
+                    var attr = this.getAttribute("data-bw-type");
+                    if( attr && attr != "text" && cfg.renderStrategies[attr]) {
+                        var ret = cfg.renderStrategies[attr].apply(this,[]);
+                        if( cfg.debug ) {
+                            var p = $(this).parent();
+                            var s = cfg.debugStyles[1];
+                            if( ret ) {
+                                p = ret.parent();
+                                s = cfg.debugStyles[0];
+                            }
+                            for( var styleProp in s ) {
+                                p.css(styleProp, s[styleProp]);
+                            }
                         }
                     }
                 }
+                $(this).bind('invalid', function() {
+                    //todo: add jQuery support for inputs w/o required property
+                });
             }
-            i++;
-        }
+        }(cfg));
     };
 })(jQuery);
